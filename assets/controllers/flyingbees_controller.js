e35f1ef89c9a77c18e1dd3318384c30c144a8023
jef-mrs <jfbarbato.dev@gmail.com>
2024-12-21 13:07:10 +0100

    make the bee fly

---
+++ assets/controllers/flyingbees_controller.js
@@ -0,0 +1,21 @@
+import { Controller } from '@hotwired/stimulus';
+
+
+export default class extends Controller {
+
+    
+    connect() {
+
+        setInterval(this.flying, 1000);
+    }
+
+    flying() {
+
+            document.getElementById('hit-btn').style['left'] = `${Math.random() * 90}%`
+            document.getElementById('hit-btn').style['top'] = `${Math.random() * 90}%`
+            // console.log('hello')
+
+        }
+    
+
+}
--- assets/controllers/hitbees_controller.js
+++ assets/controllers/hitbees_controller.js
@@ -21,11 +21,8 @@ export default class extends Controller {
                 if(data.message){
                     alert(data.message)
                 }
-        
-
-                }
-            );
-        
-        }
+            }
+        );    
+    }
 
 }
